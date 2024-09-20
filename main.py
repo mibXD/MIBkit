24ecdc43b9590ce6ffa465528ffc749201b9e1c3
ManInBlack <invisible.php@gmail.com>
2024-09-20 02:12:26 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 #!usr/bin/python3
 
 # Imports
+import os
 import sys
 
 # Banner
-print("""        
- .        :   ::::::::::.
-;;,.    ;;;  ;;; ;;;'';;'
-[[[[, ,[[[[, [[[ [[[__[[\.
-$$$$$$$$"$$$ $$$ $$''''Y$$
-888 Y88" 888o888_88o,,od8P
-MMM  M'  "MMMMMM""YUMMMP"
-
-      ToolKit v1.0""")
+def Banner():
+    print(""" 
+     
+          
+     .        :   ::::::::::.
+    ;;,.    ;;;  ;;; ;;;'';;'
+    [[[[, ,[[[[, [[[ [[[__[[\.
+    $$$$$$$$"$$$ $$$ $$''''Y$$
+    888 Y88" 888o888_88o,,od8P
+    MMM  M'  "MMMMMM""YUMMMP"
+    
+    
+          ToolKit v1.0""")
+     
+Banner()
 
 while True:
  input('>> ')
