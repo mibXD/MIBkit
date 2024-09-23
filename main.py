f4f0105424c341ea5644918472cfddbcf9f9ae48
ManInBlack <invisible.php@gmail.com>
2024-09-23 14:27:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,32 @@
 # Imports
 import os
 import sys
+import time
+
 
 # Banner
 def Banner():
-    print(""" 
-     
-          
+        print("""
+
+
      .        :   ::::::::::.
     ;;,.    ;;;  ;;; ;;;'';;'
     [[[[, ,[[[[, [[[ [[[__[[\.
-    $$$$$$$$"$$$ $$$ $$''''Y$$
+    $$$$$$$$"$$$ $$$ $$----Y$$
     888 Y88" 888o888_88o,,od8P
     MMM  M'  "MMMMMM""YUMMMP"
-    
-    
+
+
           ToolKit v1.0""")
-     
+
 Banner()
 
-while True:
- input('>> ')
+# Import console
+def main_console():
+
+
+        run_console_command = f'python3 /data/data/com.termux/files/home/MIBkit/console.py'
+        os.system(run_console_command)
+
+
+main_console()
