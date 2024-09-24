b0cb131856393e7ae448b176294d6d6f25b97bc7
ManInBlack <invisible.php@gmail.com>
2024-09-24 17:00:40 -0300

    Update trackphone.py

--- trackphone.py
+++ trackphone.py
@@ -1,8 +1,20 @@
 def Tracker():
 
 
-        import time
+        import phonenumbers
+        from phonenumbers import geocoder
+        from opencage.geocoder import OpenCageGeocode
 
 
-        print('Tracker - Ok')
-        time.sleep(0.5)
+        key = '86b3c36bfe4d4b1eab49c785386cfc8d'
+        number = input('# Set the phone number: ')
+
+
+        unumber = phonenumbers.parse(number)
+        location = geocoder.description_for_number(unumber, 'en')
+        gcoder = OpenCageGeocode(key)
+        query = str(location)
+        results = gcoder.geocode(query)
+
+
+        print(location, results)
